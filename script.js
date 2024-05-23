c59314dda30d45e7d8562c0e51fd8fa9208d5504
Yusuf <ykhan311@gmail.com>
2024-05-23 12:13:07 +0100

    update to the script file

---
+++ script.js
@@ -0,0 +1,9 @@
+const figure = ['Nicolo Machiavelli', 'Miyamoto Musashi', 'Robert Greene', 'Sun Tzu', 'Laozi', 'Rumi', 'Jordan B. Peterson']
+const theme = ['Power', 'Politics', 'Psychology', 'Strategy', 'Life Skills', 'Wisdom', 'Love']
+const advice = {
+    Power: ['Before all else, be armed.', 'Power cannot accrue to those who squander their treasure of words.'],
+    Wisdom: ['The wise man does at once what the fool does finally.'],
+    Strategy: ['Men should be treated generously or destroyed, because they take revenge for slight injuries - for heavy ones they cannot.', 'So, in war, the way is to avoid what is strong and strike at what is weak.' ],
+    Love: ['Your task is not to seek for love, but merely to seek and find all the barriers within yourself that you have built aginst it.'],
+    Politics: [],
+}
